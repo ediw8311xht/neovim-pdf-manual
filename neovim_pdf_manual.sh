c66a9b7dd97421ec539d033af0ed2d8c387ae55b
ediw8311xht <maxballard333@gmail.com>
2024-08-16 08:22:26 -0400

    updated to be posix compliant

--- neovim_pdf_manual.sh
+++ neovim_pdf_manual.sh
@@ -11,8 +11,10 @@ mkdir -p $output_dir
 echo "Downloading stuff"
 wget -r --no-parent --no-directories -P $download_dir -o log https://neovim.io/doc/user/
 echo "done downloading"
-
-for f in $(ls -1 $download_dir | sed -e 's/\.html$//'); do
-	echo "Converting $f.html to $f.pdf"
-	pandoc $download_dir/$f.html -V geometry:a4paper --pdf-engine=pdfroff -o $output_dir/$f.pdf
-done
+find "${download_dir}" -type f -exec basename -s '.html' {}  -z \; \
+    | xargs -0 -I '{}' pandoc "${download_dir}/{}.html" -V geometry:a4paper --pdf-engine=pdfroff -o "${output_dir}/{}.pdf"
+#while read -r '\0' i ; do
+#    echo "--${i}--"
+#	#echo "Converting $f.html to $f.pdf"
+#	#pandoc $download_dir/$f.html -V geometry:a4paper --pdf-engine=pdfroff -o $output_dir/$f.pdf
+#done < <()
