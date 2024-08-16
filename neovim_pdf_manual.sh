72171a7a58ef4663de993c3a3c14477a5632f211
ediw8311xht <maxballard333@gmail.com>
2024-08-16 09:14:18 -0400

    Updated

--- neovim_pdf_manual.sh
+++ neovim_pdf_manual.sh
@@ -11,7 +11,7 @@ mkdir -p $output_dir
 echo "Downloading stuff"
 wget -r --no-parent --no-directories -P $download_dir -o log https://neovim.io/doc/user/
 echo "done downloading"
-find "${download_dir}" -type f -exec basename -s '.html' {}  -z \; \
+find "${download_dir}" -type f -exec basename -z -s '.html' {} \; \
     | xargs -0 -I '{}' pandoc "${download_dir}/{}.html" -V geometry:a4paper --pdf-engine=pdfroff -o "${output_dir}/{}.pdf"
 #while read -r '\0' i ; do
 #    echo "--${i}--"
